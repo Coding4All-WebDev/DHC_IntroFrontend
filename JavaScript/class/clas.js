94a773f4bfa3e51b264e0b94110b42777d412ca2
DHC77 <DHC7654321@outlook.com>
2022-11-27 21:54:14 +0530

    commit

---
+++ JavaScript/class/clas.js
@@ -0,0 +1,86 @@
+//1..
+// class room
+//  {
+    
+//     constructor(height,lenght)
+//     {
+//         this.height = height;
+//         this.lenght= lenght;
+//     }
+
+//     gethl1()
+//      {
+    
+//         return this.height;
+//         return this.lenght;
+//     }
+    
+
+// }
+
+// let h = new room(10,20)
+// console.log(h);
+
+//2..
+
+// class employee
+//  {
+    
+//     constructor(emp_id,emp_name)
+//     {
+//         this.emp_id = emp_id;
+//         this.emp_name= emp_name;
+//     }
+
+//     gethl1()
+//      {
+    
+//         return this.emp_id;
+//         return this.emp_name;
+//     }
+    
+
+// }
+
+// let h = new employee(2282,"martin")
+// console.log(h);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function marks(stu_name,stu_marks) 
+// {
+//     this.stu_name =stu_name;
+//     this.getstu_Name ()
+//      { 
+//         return this.stu_name
+//      };
+// }
+
+
+// let c = new marks("John");
+
+// c.marks();
