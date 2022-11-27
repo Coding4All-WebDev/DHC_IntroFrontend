98387076c34ec99be961b95b4f4cd07bd3aabf48
DHC77 <DHC7654321@outlook.com>
2022-11-27 21:43:47 +0530

    commit

--- JavaScript/object/obj.js
+++ JavaScript/object/obj.js
@@ -1,12 +1,47 @@
+// 1..
+// let y = new Object();
+// let x=new Object();
+// y.emp_id = 28273;
+// x.emp_name="martin"
 
-let y = new Object();
+// y.getemp_id= function getemp_id()
+// {
+//      return this.emp_id;
+// }
+// x.getemp_name= function getname()
+// {
+//      return this.emp_name;
+// }
 
-y.name = "john";
 
-y.getName = function getName() { return this.name; }
-
-
-// how to use this object
+// console.log(y.getemp_id());
+// console.log(x.getemp_name());
 
 
-y.getName();
+// var dict =
+// {    emp_name : "john",
+//      emp_age: 24,
+//     emp_salary: 90000,
+
+//     getemp_name()
+//     { 
+//         return this.emp_name
+//     },
+//     getemp_age()
+//     {
+//         return this.emp_age;
+//     },
+//     getemp_salary()
+//     {
+//         return this.emp_salary;
+//     }
+
+// }
+
+
+// console.log(dict.getemp_name())
+// console.log(dict.getemp_age())
+// console.log(dict.getemp_salary())
+
+
+
